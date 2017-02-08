50b98c7665f924e6390abef1e6925fc68ac4ea12
okasha <okashoon@gmail.com>
2017-02-08 16:18:45 +0200

    finished task.spec.js and updated task-manager to instantiate task object from the task.js file

--- assignment11/task-manager.js
+++ assignment11/task-manager.js
@@ -1,12 +1,14 @@
+ Task = require('./task');
+
 //create is a non constructive function that creates a manager object
 var create = function(){
     
     var manager = {}
-    //tasks is a prperty of the manager object that contains an array of all created tasks
+    //array to hold tasks initiated by manager
     manager.tasks = [];
     //array that contains only active tasks
     manager.activeTasks =[];
-    //get is a function that gets a specific task from the tasks array
+    //get is a function that gets a task by its index
     manager.get = function(n){
         return this.tasks[n];
     }
@@ -35,23 +37,11 @@ var create = function(){
 
     //create is a non-constructive function that returns a task object
     manager.create = function(category, title, priority, estimate){
-        var task = {};
-        task.category = category;
-        task.title = title;
-        task.priority = priority;
-        task.estimate = estimate;
-        //initiate spent property to 0
-        task.spent = 0;
-        task.remaining = task.estimate - task.spent;
-        task.active = false;
-        //if task estimate == 0 then it is not active, else it is still active
-        if(task.estimate != 0) {
-            task.active = true;
-            this.activeTasks.push(task);
-        }
-        //push the created task object to the creating manager's task array
-        this.tasks.push(task);
-        return task;
+      var task =  new Task(category, title, priority, estimate);
+      //if estimate == 0 then task is finished and not active, else it is active
+      if(task.estimate !== 0) this.activeTasks.push(task);
+      this.tasks.push(task);
+      return task;
     }
     return manager;
 }
